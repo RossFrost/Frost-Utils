0d65d185e573899f343995eaa5bb9855ce7a37a1
Frost-Ross <101983571+RossFrost@users.noreply.github.com>
2022-04-29 10:21:35 -0500

    Update frost-utils.js

--- frost-utils.js
+++ frost-utils.js
@@ -1,16 +1,40 @@
-
 /**
  * @param {document.querySelector} element 
  */
+
 
 function checkElementExistence(element) {
     if (typeof element != "undefined") {
         return true
     }
 
-    else {
-        return false
+    return false
+}
+
+
+function query_select(element, scope = "none", half_options = "ceil") {
+    switch (scope) {
+        case "none":
+            if (Array.isArray(element) == true) {
+                return document.querySelector(element.toString())}
+            return document.querySelector(element)
+
+        case "all":
+            return document.querySelectorAll(element)
+
+        case "middle":
+            function halves_maths(length, default_ = "array") {
+                let half = (length.length / 2) - 1
+                if (half_options == "floor") {return Math.floor(half)}
+                return Math.ceil(half)}
+                
+            if (scope = "all") {document.querySelectorAll(element[halves_maths(element)])}
+            console.log(scope + " has to be set to all")
+
+
     }
+
+    console.log(scope + " is an invalid scope.")
 }
 
 
@@ -20,22 +44,20 @@ function checkElementExistence(element) {
  * @param {string} location Elements location in document
  */
  
-function creatElements(elements, location) {
-    for (element of elements) {
-        console.log(element)
-        let created_element = document.createElement(element[0])
+function create_elements(elements, location) {
+    for (var element of elements) {
+        var  created_element = document.createElement(element[0]); var key; var value;
 
         for ([key, value] of Object.entries(element[1])) {
-            console.log(key, value)
-            let bin = created_element.setAttribute(key, value)
-        }
+            if (key == "text") {created_element.appendChild(document.createTextNode(value)); continue}
+            let bin = created_element.setAttribute(key, value)}
         location.append(created_element)
     }
 }
 
 /**
  * 
- * @param {string} dependents A liste of dependents to wait to load
+ * @param {string} dependents A list of dependents to wait to load
  * @returns "ready"
  */
 
@@ -58,7 +80,7 @@ function load_dependents(dependents) {
 
 
 function get_URL() {
-    return window.location + "//" + window.location.protocol
+    return window.location.origin
 }
 
 
@@ -70,8 +92,7 @@ function get_URL() {
 
 
 function single_listener(element, event_, func) {
-    let queried_element = document.querySelector(element) 
-    let event__ = queried_element.addEventListener(event_, func)
+    let event__ = query_select(element).addEventListener(event_, func)
     return event__
 }
 
@@ -85,11 +106,24 @@ function mutli_listener(elements, func) {
     for (item in set) {
         let element = elements[item][0]
         let event_ = elements[item][1]
-        let element_ = document.querySelector(element)
+        let list_of_listeners = []
 
         if (checkElementExistence(element) == true) {
-            let event__ = queried_element.addEventListener(event_, func)
-
+            let event_listener = query_select(element).addEventListener(event_, func)
+            list_of_listeners.push(event_listener)
         }
     }
+
+    return list_of_listeners
 }
+
+export {
+        checkElementExistence, 
+        query_select, mutli_listener, 
+        single_listener, get_URL, 
+        load_dependents,
+        create_elements
+       };
+
+
+                   

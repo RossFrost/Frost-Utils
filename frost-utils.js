a262d704ba7bb8cabd41b664677146d532e054e7
Frost-Ross <101983571+RossFrost@users.noreply.github.com>
2022-05-03 07:39:56 -0700

    Consistent variable naming

--- frost-utils.js
+++ frost-utils.js
@@ -1,27 +1,26 @@
 
 //syntax: [[element, {attribute: value], ...]
-function create_elements(elements, location = "none") {
+function CREATE_ELEMENTS(elements, location = "none") {
     for (var element of elements) {
-        var created_element = document.createElement(element[0])
+        var createdElement = document.createElement(element[0]) // createElement -> function; not createdElement -> variable
         var key; var value;
         let location_ = [false, location]
 
         for ([key, value] of Object.entries(element[1])) {
             if (key == "text") {
-                created_element.appendChild(document.createTextNode(value)); continue}
+                createdElement.appendChild(document.createTextNode(value)); continue}
             else if (key == "location") {
                 location_ = [true, value]; continue}
 
-            created_element.setAttribute(key, value)}
+            createdElement.setAttribute(key, value)}
 
         if (location_[0] = true && location_[1] != "none") {
-            location_[1].append(created_element); continue}
+            location_[1].append(createdElement); continue}
 
-        location_[1].append(created_element)
-    }
+        location_[1].append(createdElement)}
 }
 
 
 export {
-        create_elements
+        CREATE_ELEMENTS
        }
